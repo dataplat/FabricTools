076cda7515c5df6f5b8038267140599899c992c2
jpomfret <jpomfret7@gmail.com>
2025-05-14 09:51:18 +0100

    fix for #25 add config variables to private function so executed on build

---
+++ source/Private/Set-FabConfig.ps1
@@ -0,0 +1,37 @@
+# this is a workaround to get the variables set for now
+# TODO: change to use PSFConfig?
+
+$script:FabricSession = [ordered]@{
+    BaseApiUrl   = 'https://api.fabric.microsoft.com/v1'
+    ResourceUrl  = 'https://api.fabric.microsoft.com'
+    FabricToken  = $null
+    HeaderParams = $null
+    ContentType  = @{'Content-Type' = "application/json" }
+    KustoURL     = "https://api.kusto.windows.net"
+    AccessToken  = $null
+}
+
+$script:AzureSession = [ordered]@{
+    BaseApiUrl   = "https://management.azure.com"
+    AccessToken  = $null
+    Token        = $null
+    HeaderParams = $null
+}
+
+$script:PowerBI = [ordered]@{
+    BaseApiUrl = "https://api.powerbi.com/v1.0/myorg"
+}
+
+$FabricTools = @{
+    FeatureFlags = @{
+        AutoRenewExpiredToken = $true
+    }
+}
+
+$FabricConfig = @{
+    BaseUrl        = "https://api.fabric.microsoft.com/v1"
+    ResourceUrl    = "https://api.fabric.microsoft.com"
+    FabricHeaders  = @{}
+    TenantIdGlobal = ""
+    TokenExpiresOn = ""
+}
