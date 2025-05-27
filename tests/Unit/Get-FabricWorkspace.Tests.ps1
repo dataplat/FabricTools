7d2271e9269f371ba4f64344a275358ba6fcf6b9
jpomfret <jpomfret7@gmail.com>
2025-05-27 18:05:12 +0100

    remove validation test as we no longer validate

--- tests/Unit/Get-FabricWorkspace.Tests.ps1
+++ tests/Unit/Get-FabricWorkspace.Tests.ps1
@@ -43,17 +43,4 @@ Describe "Get-FabricWorkspace" -Tag "UnitTests" {
             Compare-Object -ReferenceObject $expected -DifferenceObject $hasparms | Should -BeNullOrEmpty
         }
     }
-
-    Context "WorkspaceName parameter validation" {
-        It "Throws error when WorkspaceName does not match ValidatePattern" {
-            # Assuming the ValidatePattern allows only alphanumeric, underscore, space and hyphen
-            { Get-FabricWorkspace -WorkspaceName "InvalidName!" } | Should -Throw
-            { Get-FabricWorkspace -WorkspaceName "Another@Invalid" } | Should -Throw
-        }
-
-        It "Does not throw when WorkspaceName matches ValidatePattern" {
-            { Get-FabricWorkspace -WorkspaceName "Valid_Name-123" } | Should -Not -Throw
-            { Get-FabricWorkspace -WorkspaceName "Another Valid Name" } | Should -Not -Throw
-        }
-    }
 }
