6cb3a0656f98fb01b64bf5687ca20749a8082523
Rob Sewell <mrrobsewell@outlook.com>
2025-06-10 13:51:37 +0100

    Just another deployment (#112)

--- source/Public/Config/Set-FabricConfig.ps1
+++ source/Public/Config/Set-FabricConfig.ps1
@@ -25,7 +25,7 @@ Registers the specified Fabric Data Warehouse configuration for use with all fun
 .EXAMPLE
 PS> Set-FabricConfig -WorkspaceGUID 'GUID-GUID-GUID-GUID' -DataWarehouseGUID 'GUID-GUID-GUID-GUID' -SkipPersist
 
-Registers the specified Fabric Data Warehouse configuration for use with all functions in the PSFabricTools module - but does not persist the values, only uses them for the current session.
+Registers the specified Fabric Data Warehouse configuration for use with all functions in the PSFabricTools module - but it does not persist the values, only uses them for the current session.
 #>
 
 function Set-FabricConfig {
