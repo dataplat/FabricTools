b93333d8aa0ccb803ca577f6449c6535624bb023
Rob Sewell <mrrobsewell@outlook.com>
2025-06-11 11:43:49 +0100

    Refactor Get-FabricConfig function structure
    Reorganize the Get-FabricConfig function to improve readability and maintainability. The function header is now properly defined, and the author note is formatted consistently.
    
    Thank you!

--- source/Public/Config/Get-FabricConfig.ps1
+++ source/Public/Config/Get-FabricConfig.ps1
@@ -1,4 +1,5 @@
-<#
+function Get-FabricConfig {
+    <#
 .SYNOPSIS
 Gets the configuration for use with all functions in the PSFabricTools module.
 
@@ -20,12 +21,9 @@ Gets the BaseUrl configuration value for the PSFabricTools module.
 
 .NOTES
 
-Author: Jess Pomfret
+    Author: Jess Pomfret
 
 #>
-
-
-function Get-FabricConfig {
     param (
         [String]$ConfigName
     )
