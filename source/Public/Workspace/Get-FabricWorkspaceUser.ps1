7b212fa570e43fc29bcec5511e0ea154d2082bca
Rob Sewell <mrrobsewell@outlook.com>
2025-06-15 12:43:05 +0100

    Rename function, improve examples and add alias

--- source/Public/Workspace/Get-FabricWorkspaceUser.ps1
+++ source/Public/Workspace/Get-FabricWorkspaceUser.ps1
@@ -1,28 +1,30 @@
-function Get-FabricWorkspaceUsers {
+function Get-FabricWorkspaceUser {
     <#
 .SYNOPSIS
-Retrieves the users of a workspace.
+Retrieves the user(s) of a workspace.
 
 .DESCRIPTION
-The Get-FabricWorkspaceUsers function retrieves the users of a workspace. It supports multiple aliases for flexibility.
+The Get-FabricWorkspaceUser function retrieves the details of the users of a workspace.
 
 .PARAMETER WorkspaceId
-The ID of the workspace. This is a mandatory parameter for the 'WorkspaceId' parameter set.
+The ID of the workspace. This is a mandatory parameter if Workspace is not provided.
 
 .PARAMETER Workspace
-The workspace object. This is a mandatory parameter for the 'WorkspaceObject' parameter set and can be piped into the function.
+The workspace object. This normally comes from the Get-FabricWorkspace cmdlet. This is a mandatory parameter if WorkspaceId is not provided.
 
 .EXAMPLE
-Get-FabricWorkspaceUsers -WorkspaceId "your-workspace-id"
+Get-FabricWorkspaceUser -WorkspaceId '12345678-1234-1234-1234-123456789012
 
-This example retrieves the users of a workspace given the workspace ID.
+This example retrieves the users of a workspace of the workspace with the ID '12345678-1234-1234-1234-123456789012'.
 
 .EXAMPLE
-$workspace | Get-FabricWorkspaceUsers
+$workspace = Get-FabricWorkspace -WorkspaceName 'prod-workspace'
+$workspace | Get-FabricWorkspaceUser
 
-This example retrieves the users of a workspace given a workspace object.
+This example retrieves the users of the prod-workspace workspace by piping the object.
 
 .NOTES
+It supports multiple aliases for flexibility.
 The function defines parameters for the workspace ID and workspace object. If the parameter set name is 'WorkspaceId', it retrieves the workspace object. It then makes a GET request to the PowerBI API to retrieve the users of the workspace and returns the 'value' property of the response, which contains the users.
 
 Author: Ioana Bouariu
@@ -32,6 +34,7 @@ Author: Ioana Bouariu
     # This function retrieves the users of a workspace.
     # Define aliases for the function for flexibility.
     [Alias("Get-FabWorkspaceUsers")]
+    [Alias("Get-FabricWorkspaceUsers")]
 
     # Define parameters for the workspace ID and workspace object.
     param(
