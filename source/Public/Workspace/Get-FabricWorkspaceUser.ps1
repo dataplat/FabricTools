7da16e91396813c003e9cc49b4e1566efbb00163
Rob Sewell <mrrobsewell@outlook.com>
2025-06-15 14:05:33 +0100

    Refactor Get-FabricWorkspaceUserto enable processing via the pipeline  #75
    Updated the Get-FabricWorkspaceUser function to enhance readability and maintainability. Added additional examples to the documentation for better user guidance. Improved the process block to handle multiple workspaces and return user details more effectively.
    
    Thank you!

--- source/Public/Workspace/Get-FabricWorkspaceUser.ps1
+++ source/Public/Workspace/Get-FabricWorkspaceUser.ps1
@@ -1,4 +1,5 @@
-function Get-FabricWorkspaceUser {
+function Get-FabricWorkspaceUser
+{
     <#
 .SYNOPSIS
 Retrieves the user(s) of a workspace.
@@ -23,8 +24,25 @@ $workspace | Get-FabricWorkspaceUser
 
 This example retrieves the users of the prod-workspace workspace by piping the object.
 
+.EXAMPLE
+Get-FabricWorkspaceUser -Workspace (Get-FabricWorkspace -WorkspaceName 'prod-workspace')
+
+This example retrieves the users of the prod-workspace workspace by piping the object.
+
+.EXAMPLE
+Get-FabricWorkspace| Get-FabricWorkspaceUser
+
+This example retrieves the users of all of the workspaces. IMPORTANT: This will not return the workspace name or ID at present.
+
+.EXAMPLE
+Get-FabricWorkspace| Foreach-Object {
+Get-FabricWorkspaceUser -WorkspaceId $_.Id | Select-Object @{Name='WorkspaceName';Expression={$_.displayName;}}, *
+}
+
+This example retrieves the users of all of the workspaces. It will also add the workspace name to the output.
+
 .NOTES
-It supports multiple aliases for flexibility.
+It supports multiple aliases for backward compatibility.
 The function defines parameters for the workspace ID and workspace object. If the parameter set name is 'WorkspaceId', it retrieves the workspace object. It then makes a GET request to the PowerBI API to retrieve the users of the workspace and returns the 'value' property of the response, which contains the users.
 
 Author: Ioana Bouariu
@@ -42,22 +60,42 @@ Author: Ioana Bouariu
         [string]$WorkspaceId,
 
         [Parameter(Mandatory = $true, ParameterSetName = 'WorkspaceObject', ValueFromPipeline = $true )]
+        [object[]]
         $Workspace
     )
 
-    begin {
+    begin
+    {
         Confirm-TokenState
+        # If the parameter set name is 'WorkspaceId', return the workspace object so that it can be used in the process block.
+        if ($PSCmdlet.ParameterSetName -eq 'WorkspaceId')
+        {
+            $Workspace = Get-FabricWorkspace -WorkspaceId $WorkspaceId
+        }
+        $returnValue = @()
     }
 
-    process {
-        # If the parameter set name is 'WorkspaceObject', retrieve the workspace object.
-        if ($PSCmdlet.ParameterSetName -eq 'WorkspaceObject') {
-            $WorkspaceId = $Workspace.id
+    process
+    {
+
+        foreach ($space in $Workspace)
+        {
+
+            $Uri = "groups/{0}/users" -f $space.id
+
+            # Make a GET request to the PowerBI API to retrieve the users of the workspace.
+            # The function returns the 'value' property of the response, which contains the users.
+            $returnValue += (Invoke-FabricRestMethod -Method get -PowerBIApi -uri $Uri).value
         }
 
-        # Make a GET request to the PowerBI API to retrieve the users of the workspace.
-        # The function returns the 'value' property of the response, which contains the users.
-        return (Invoke-FabricRestMethod -Method get -PowerBIApi -uri ("groups/$WorkspaceId/users")).value
     }
-
+    end
+    {
+        # We should not be using the return keyword.
+        # This will enable future improvements such as adding additional value to the returned object such as the workspace name and better column headings
+        # It will also allow for future improvements such as filtering the returned object.
+        # Uses the code as defined https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_methods?view=powershell-7.5&viewFallbackFrom=powershell-7.2#input-processing-methods
+        # Return the users of the workspace.
+        $returnValue
+    }
 }
