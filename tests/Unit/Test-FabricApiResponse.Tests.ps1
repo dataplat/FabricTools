b265a43ed30a996da46ccb02ad7c14b14bf6a38b
Kamil Nowinski <kamil@nowinski.net>
2025-06-17 21:51:43 +0100

    Refactor Test-FabricApiResponse for parameter validation and more unit tests for the function.

--- tests/Unit/Test-FabricApiResponse.Tests.ps1
+++ tests/Unit/Test-FabricApiResponse.Tests.ps1
@@ -1,49 +1,106 @@
-# #Requires -Module @{ ModuleName="Pester"; ModuleVersion="5.0"}
-# param(
-#     $ModuleName = "FabricTools",
-#     $expectedParams = @(
-#         "statusCode"
-#                 "response"
-#                 "responseHeader"
-#                 "Name"
-#                 "typeName"
-#                 "Verbose"
-#                 "Debug"
-#                 "ErrorAction"
-#                 "WarningAction"
-#                 "InformationAction"
-#                 "ProgressAction"
-#                 "ErrorVariable"
-#                 "WarningVariable"
-#                 "InformationVariable"
-#                 "OutVariable"
-#                 "OutBuffer"
-#                 "PipelineVariable"
+#Requires -Module @{ ModuleName="Pester"; ModuleVersion="5.0"}
 
-#     )
-# )
+InModuleScope FabricTools {
 
-# Describe "Test-FabricApiResponse" -Tag "UnitTests" {
+param(
+    $ModuleName = "FabricTools",
+    $expectedParams = @(
+        "Response"
+        "Name"
+        "TypeName"
+        "NoWait"
+        "Verbose"
+        "Debug"
+        "ErrorAction"
+        "WarningAction"
+        "InformationAction"
+        "ProgressAction"
+        "ErrorVariable"
+        "WarningVariable"
+        "InformationVariable"
+        "OutVariable"
+        "OutBuffer"
+        "PipelineVariable"
+    )
+)
 
-#     BeforeDiscovery {
-#         $command = Get-Command -Name Test-FabricApiResponse
-#         $expected = $expectedParams
-#     }
+Describe "Test-FabricApiResponse" -Tag "UnitTests" {
 
-#     Context "Parameter validation" {
-#         BeforeAll {
-#             $command = Get-Command -Name Test-FabricApiResponse
-#             $expected = $expectedParams
-#         }
+    BeforeDiscovery {
+        ipmo ".\output\module\FabricTools\0.0.1\FabricTools.psd1"
 
-#         It "Has parameter: <_>" -ForEach $expected {
-#             $command | Should -HaveParameter $PSItem
-#         }
+        $command = Get-Command -Name Test-FabricApiResponse
+        $script:expected = $expectedParams
+    }
 
-#         It "Should have exactly the number of expected parameters $($expected.Count)" {
-#             $hasparms = $command.Parameters.Values.Name
-#             #$hasparms.Count | Should -BeExactly $expected.Count
-#             Compare-Object -ReferenceObject $expected -DifferenceObject $hasparms | Should -BeNullOrEmpty
-#         }
-#     }
-# }
+    Context "Parameter validation" {
+        BeforeAll {
+            $command = Get-Command -Name Test-FabricApiResponse
+            $expected = $expectedParams
+        }
+
+        It "Has parameter: <_>" -ForEach $expected {
+            $command | Should -HaveParameter $PSItem
+        }
+
+        It "Should have exactly the number of expected parameters $($expected.Count)" {
+            $hasparms = $command.Parameters.Values.Name
+            Compare-Object -ReferenceObject $script:expected -DifferenceObject $hasparms | Should -BeNullOrEmpty
+        }
+    }
+}
+
+
+Describe "Test-FabricApiResponse - StatusCode Handling" -Tag "UnitTests" {
+
+    BeforeAll {
+        # Generate a random GUID for x-ms-operation-id
+        $script:responseHeader = @{ "x-ms-operation-id" = [guid]::NewGuid().ToString() }
+        $script:response = @{ "foo" = "bar" }
+    }
+
+    BeforeEach {
+        # Always mock these functions
+        Mock -CommandName Get-FabricLongRunningOperation -MockWith {
+            return @{ "status" = "Succeeded" }
+        }
+        Mock -CommandName Get-FabricLongRunningOperationResult -MockWith {
+            return "Completed"
+        }
+        Mock -CommandName Write-Message -MockWith { }
+    }
+
+    It "Returns `$null when statusCode is 200" {
+        $script:statusCode = 200
+        $result = Test-FabricApiResponse -Response $script:response
+        $result | Should -Be $null
+    }
+
+    It "Returns response when statusCode is 201" {
+        $script:statusCode = 201
+        $result = Test-FabricApiResponse -Response $script:response
+        $result | Should -Be $script:response
+    }
+
+    It "Returns operation result when statusCode is 202 and operation succeeds" {
+        $script:statusCode = 202
+        $expectedResult = "Completed"
+        $result = Test-FabricApiResponse -Response $script:response
+        $result | Should -Be $expectedResult
+        Should -Invoke Get-FabricLongRunningOperation -Exactly 1
+        Should -Invoke Get-FabricLongRunningOperationResult -Exactly 1
+    }
+
+    It "Returns responseHeader when statusCode is 202 and -NoWait is specified" {
+        $script:statusCode = 202
+        $result = Test-FabricApiResponse -Response $script:response -NoWait
+        $result | Should -Be $script:responseHeader
+    }
+
+    It "Throws when statusCode is not 200, 201, or 202" {
+        $script:statusCode = 400
+        { Test-FabricApiResponse -Response $script:response -ErrorAction Stop } | Should -Throw
+    }
+}
+
+}
