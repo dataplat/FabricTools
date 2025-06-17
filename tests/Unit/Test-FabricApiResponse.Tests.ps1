185ee6dca7d5f868681c7b9c873b9d38ecc17074
Kamil Nowinski <kamil@nowinski.net>
2025-06-17 02:36:20 +0100

    This function is moved to private now

--- tests/Unit/Test-FabricApiResponse.Tests.ps1
+++ tests/Unit/Test-FabricApiResponse.Tests.ps1
@@ -1,50 +1,49 @@
-#Requires -Module @{ ModuleName="Pester"; ModuleVersion="5.0"}
-param(
-    $ModuleName = "FabricTools",
-    $expectedParams = @(
-        "statusCode"
-                "response"
-                "responseHeader"
-                "Name"
-                "typeName"
-                "Verbose"
-                "Debug"
-                "ErrorAction"
-                "WarningAction"
-                "InformationAction"
-                "ProgressAction"
-                "ErrorVariable"
-                "WarningVariable"
-                "InformationVariable"
-                "OutVariable"
-                "OutBuffer"
-                "PipelineVariable"
-                
-    )
-)
+# #Requires -Module @{ ModuleName="Pester"; ModuleVersion="5.0"}
+# param(
+#     $ModuleName = "FabricTools",
+#     $expectedParams = @(
+#         "statusCode"
+#                 "response"
+#                 "responseHeader"
+#                 "Name"
+#                 "typeName"
+#                 "Verbose"
+#                 "Debug"
+#                 "ErrorAction"
+#                 "WarningAction"
+#                 "InformationAction"
+#                 "ProgressAction"
+#                 "ErrorVariable"
+#                 "WarningVariable"
+#                 "InformationVariable"
+#                 "OutVariable"
+#                 "OutBuffer"
+#                 "PipelineVariable"
 
-Describe "Test-FabricApiResponse" -Tag "UnitTests" {
+#     )
+# )
 
-    BeforeDiscovery {
-        $command = Get-Command -Name Test-FabricApiResponse
-        $expected = $expectedParams
-    }
+# Describe "Test-FabricApiResponse" -Tag "UnitTests" {
 
-    Context "Parameter validation" {
-        BeforeAll {
-            $command = Get-Command -Name Test-FabricApiResponse
-            $expected = $expectedParams
-        }
+#     BeforeDiscovery {
+#         $command = Get-Command -Name Test-FabricApiResponse
+#         $expected = $expectedParams
+#     }
 
-        It "Has parameter: <_>" -ForEach $expected {
-            $command | Should -HaveParameter $PSItem
-        }
+#     Context "Parameter validation" {
+#         BeforeAll {
+#             $command = Get-Command -Name Test-FabricApiResponse
+#             $expected = $expectedParams
+#         }
 
-        It "Should have exactly the number of expected parameters $($expected.Count)" {
-            $hasparms = $command.Parameters.Values.Name
-            #$hasparms.Count | Should -BeExactly $expected.Count
-            Compare-Object -ReferenceObject $expected -DifferenceObject $hasparms | Should -BeNullOrEmpty
-        }
-    }
-}
+#         It "Has parameter: <_>" -ForEach $expected {
+#             $command | Should -HaveParameter $PSItem
+#         }
 
+#         It "Should have exactly the number of expected parameters $($expected.Count)" {
+#             $hasparms = $command.Parameters.Values.Name
+#             #$hasparms.Count | Should -BeExactly $expected.Count
+#             Compare-Object -ReferenceObject $expected -DifferenceObject $hasparms | Should -BeNullOrEmpty
+#         }
+#     }
+# }
