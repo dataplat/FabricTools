ad6c6e028e43f383e638b1ec8b401ccce30602ab
Kamil Nowinski <kamil@nowinski.net>
2025-07-04 21:05:19 +0100

    Generate documentation to markdown format #9

---
+++ helper/CreateDocumentation.ps1
@@ -0,0 +1,13 @@
+Import-Module -Name platyPS
+
+$mdHelpParams = @{
+    Module                = 'FabricTools'
+    OutputFolder          = ".\documentation"
+    AlphabeticParamsOrder = $true
+    UseFullTypeName       = $true
+    WithModulePage        = $true
+    ExcludeDontShow       = $false
+    Encoding              = [System.Text.Encoding]::UTF8
+    ModulePagePath        = ".\documentation\readme.md"
+}
+New-MarkdownHelp @mdHelpParams
